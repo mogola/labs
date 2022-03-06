09ab9ad598f4555e21cf368b923eba97f9cedf86
FSAUSSAYE\FSaussaye <fab.saussaye@gmail.com>
2022-03-06 20:24:57 +0100

    modif config

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'astrolune' );
+define( 'DB_NAME', 'labs' );
 
 /** MySQL database username */
 define( 'DB_USER', 'root' );
@@ -29,7 +29,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost:3306' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
