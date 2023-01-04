bb7064aa29049b60175d4031a3cbc67a98de3a20
smogola <mogola.sangare@external.loccitane.com>
2023-01-04 22:37:41 +0100

    [UPDATE] adjustment graphique

--- wp-content/themes/quartier/template-parts/page/content-page.php
+++ wp-content/themes/quartier/template-parts/page/content-page.php
@@ -103,36 +103,6 @@ has_post_thumbnail()
         ?>
 		 <!--Sponsors Section-->
          <?php get_template_part( 'include/banner', 'offer' ); ?>
-    <section class="sponsors-section">
-        <div class="auto-container">
-            <div class="slider-outer">
-                <!--Sponsors Slider-->
-                <ul class="sponsors-slider">
-                    <?php
-                   $args = array(
-                        'category_name' => 'partner'
-                   );
-                   $query = new WP_Query($args);
-                    if ( $query->have_posts() ) :
-
-                        /* Start the Loop */
-                        while ( $query->have_posts() ) : $query->the_post();
-
-                            /*
-                             * Include the Post-Format-specific template for the content.
-                             * If you want to override this in a child theme, then include a file
-                             * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-                             */
-                            get_template_part( 'template-parts/post/content', 'partner' );
-
-                        endwhile;
-
-                    endif;
-                ?>
-                </ul>
-            </div>            
-        </div>
-    </section> 
 	</div>
 </div>
 
